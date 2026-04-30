c1e7cc7fd8a07877e89e411523f03e371db11ec2
yarbework <yarbemohaz3517@gmail.com>
2026-04-30 22:27:23 +0300

    adding vercel.json for configuring backend to ddeploy on vercel

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: '*', 
+  // origin: 'http://localhost:5173', 
   credentials: true,               
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
